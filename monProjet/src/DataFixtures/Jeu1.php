ddd4cb01c2d76dec5281c9e77723b0ead3482eb2
MBIMBA Triomphe <mbimbatriomphe@gmail.com>
2024-08-22 15:42:02 +0200

    Ajout de deux commentaires dans le fichier jeu1 pour les fixtures (indiquer l'artist d'un disc et ajouter un disc à l'artist).

---
+++ monProjet/src/DataFixtures/Jeu1.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use App\Entity\Artist;
+use App\Entity\Disc;
+
+class Jeu1 extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+        $artist1 = new Artist();
+
+        $artist1->setName("Queens Of The Stone Age");
+        $artist1->setUrl("https://qotsa.com/");
+        
+        $manager->persist($artist1);
+
+
+        $disc1 = new Disc();
+        $disc1->setTitle("Songs for the Deaf");
+        $disc1->setPicture("https://en.wikipedia.org/wiki/Songs_for_the_Deaf#/media/File:Queens_of_the_Stone_Age_-_Songs_for_the_Deaf.png");
+        $disc1->setLabel("Interscope Records");
+        
+        $manager->persist($disc1);
+        
+        // Pour associer vos entités
+        $disc1->setArtist($artist1); // Pour indiquer l'Artist d'un Disc
+        // ou 
+        $artist1->addDisc($disc1);  // Pour ajouter un Disc à l'Artist
+
+
+        $manager->flush();
+    }
+}
